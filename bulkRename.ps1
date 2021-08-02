bf0496910a174590f216a747fbef7cfd0646f224
Serge Merzavets <merzavets@gmail.com>
2021-08-03 01:21:49 +0300

    готово: сокращённое форм. имени = 2do: ext intact

--- bulkRename.ps1
+++ bulkRename.ps1
@@ -1,7 +1,7 @@
 Get-ChildItem '..\..\..\' -Recurse | Where-Object {
     if (($_.Name).Length -gt 20) {
         $salt =Get-Random -Minimum 0x10001 -Maximum 0xfffff 
-        $nuName =  "{0}\{1}[{2:X}]{3}" -f $_.DirectoryName, ($_.Name).Substring(0, 10), $salt, $_.Extension 
+        $nuName =  "{0}\{1}[{2:X}]{3}" -f $_.DirectoryName, ($_.Name).Substring(0, 7), $salt, ($_.Name).Substring($_.Name.Length-9, 9), $_.Extension 
         Rename-Item $_.FullName -NewName $nuName -WhatIf
     }
 }
