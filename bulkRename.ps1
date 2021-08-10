1b34cdb51bf95a42b0061dfb40f0e2cd7d7f89bb
Merzavets <merzavets@gmail.com>
2021-08-10 12:41:27 +0300

    add longname generator (for debug)

--- bulkRename.ps1
+++ bulkRename.ps1
@@ -1,7 +1,16 @@
-Get-ChildItem '..\..\..\' -Recurse | Where-Object {
+$workFolder = 'C:\Users\Serge\Documents\Killme\longnames'
+
+for ($i = 0; $i -lt 10; $i++) {
+    $fnm = Get-Random -Minimum 0x1000000000000 -Maximum 0xfffffffffffff
+    $xtn = Get-Random -Minimum 0x10 -Maximum 0xffffff
+    New-Item -Path $workFolder -Name ("{0:x}.{1:x}" -f $fnm, $xtn)
+}
+
+Get-ChildItem $workFolder -Recurse | Where-Object {
     if (($_.Name).Length -gt 20) {
         $salt =Get-Random -Minimum 0x10001 -Maximum 0xfffff 
         $nuName =  "{0}\{1}[{2:X}]{3}" -f $_.DirectoryName, ($_.Name).Substring(0, 7), $salt, ($_.Name).Substring($_.Name.Length-9, 9), $_.Extension 
         Rename-Item $_.FullName -NewName $nuName -WhatIf
     }
 }
+
